d177c621d0716fbeabd037def99af3b2f18bd8dc
Paul <paul@theapplegates.com>
2022-05-10 21:49:45 -0400

    first commit

---
+++ components/ContentBlocks.js
@@ -0,0 +1,141 @@
+export const RenderBlocks = ({ blocks }) => {
+  return blocks.map((block) => {
+    const { type, id } = block
+    const value = block[type]
+
+    switch (type) {
+      case 'paragraph':
+        return <Text text={value.rich_text} id={id} key={id} />
+
+      case 'heading_1':
+        return <Heading text={value.rich_text} id={id} level={type} key={id} />
+
+      case 'heading_2':
+        return <Heading text={value.rich_text} id={id} level={type} key={id} />
+
+      case 'heading_3':
+        return <Heading text={value.rich_text} id={id} level={type} key={id} />
+
+      case 'bulleted_list_item':
+      case 'numbered_list_item':
+        return <ListItem key={id} value={value} id={id} />
+
+      case 'to_do':
+        return <ToDo key={id} value={value} />
+
+      case 'toggle':
+        return <Toggle key={id} value={value} />
+
+      case 'image':
+        const imageSrc = value.type === 'external' ? value.external.url : value.file.url
+        const caption = value.caption.length ? value.caption[0].plain_text : ''
+        return (
+          <figure key={id}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img alt={caption} src={imageSrc} />
+            {caption && <figcaption className="mt-2">{caption}</figcaption>}
+          </figure>
+        )
+
+      default:
+        return `Unsupported block (${type === 'unsupported' ? 'unsupported by Notion API' : type})`
+    }
+  })
+}
+
+const SpanText = ({ text, id }) => {
+  if (!text) return null
+
+  return text.map((value, i) => {
+    const {
+      annotations: { bold, code, color, italic, strikethrough, underline },
+      text,
+    } = value
+    return (
+      <span
+        key={id + i}
+        className={[
+          bold ? 'font-bold' : '',
+          code ? 'bg-gray-100 p-1 font-mono text-sm rounded-md' : '',
+          italic ? 'italic' : '',
+          strikethrough ? 'line-through' : '',
+          underline ? 'underline' : '',
+        ].join(' ')}
+        style={color !== 'default' ? { color } : {}}
+      >
+        {text.link ? (
+          <a href={text.link.url} className="underline">
+            {text.content}
+          </a>
+        ) : (
+          text.content
+        )}
+      </span>
+    )
+  })
+}
+
+const Text = ({ text, id }) => {
+  return (
+    <p className="mb-4 text-gray-700">
+      <SpanText text={text} id={id} />
+    </p>
+  )
+}
+
+const ListItem = ({ value, id }) => {
+  return (
+    <li>
+      <SpanText text={value.rich_text} id={id} />
+    </li>
+  )
+}
+
+const Heading = ({ text, level }) => {
+  switch (level) {
+    case 'heading_1':
+      return (
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight my-2 text-black">
+          <SpanText text={text} />
+        </h1>
+      )
+    case 'heading_2':
+      return (
+        <h2 className="font-bold text-2xl md:text-3xl tracking-tight my-2 text-black">
+          <SpanText text={text} />
+        </h2>
+      )
+    case 'heading_3':
+      return (
+        <h3 className="font-bold text-lg md:text-xl tracking-tight my-2 text-black">
+          <SpanText text={text} />
+        </h3>
+      )
+    default:
+      return null
+  }
+}
+
+const ToDo = ({ id, value }) => {
+  return (
+    <div>
+      <label htmlFor={id}>
+        <input type="checkbox" id={id} defaultChecked={value.checked} />{' '}
+        <SpanText text={value.rich_text} />
+      </label>
+    </div>
+  )
+}
+
+const Toggle = ({ value }) => {
+  return (
+    <details>
+      <summary className="cursor-pointer">{value.rich_text[0].text.content}</summary>
+      {value.children?.map((block) => {
+        if (block.type === 'paragraph') {
+          return <Text key={block.id} text={block.paragraph.rich_text} id={block.id} />
+        }
+      })}
+    </details>
+  )
+}
